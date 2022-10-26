b87d299929b7a274c34f75984a27a5c1f404c044
3093063838 <3093063838@users.noreply.github.com>
2022-10-26 23:54:11 +0800

    补补补！

---
+++ Assets/Scripts/Game/GridManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager
+{
+    public static GridManager Inst { get; } = new GridManager();
+
+    private Dictionary<GameObject, BaseBuilding> buildingsMap = new Dictionary<GameObject, BaseBuilding>();
+    private readonly GameObject gridsParent;
+
+    public GridManager()
+    {
+        gridsParent = new GameObject("Grids Parent");
+    }
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Init()
+    {
+        var trs = GameObject.Find("GridsConfig").transform;
+        GameObject obj;
+        BoxCollider2D col;
+        for (int i = 0; i < trs.childCount; i++)
+        {
+            obj = new GameObject("Grid");
+            obj.layer = LayerUtility.Grid;
+            obj.transform.SetParent(gridsParent.transform);
+            obj.transform.position = trs.GetChild(i).position;
+            col = obj.AddComponent<BoxCollider2D>();
+            col.size = new Vector2(1.12f, 1.12f);
+            col.offset = Vector2.zero;
+            col.isTrigger = true;
+            trs.GetChild(i).gameObject.SetActive(false);
+            buildingsMap[obj] = null;
+        }
+    }
+
+    /// <summary>
+    /// 检查格子是否可用
+    /// </summary>
+    public bool DetectGridEnable(Vector2 worldPos, out GameObject gridKey)
+    {
+        gridKey = null;
+        RaycastHit2D hitInfo = Physics2D.Raycast(worldPos, Vector2.right, 0.001f, 1 << LayerUtility.Grid);
+        if (hitInfo.collider == null)
+            return false;
+
+        gridKey = hitInfo.collider.gameObject;
+        return buildingsMap.TryGetValue(gridKey, out BaseBuilding b) && b == null;
+    }
+
+    /// <summary>
+    /// 建筑占用格子
+    /// </summary>
+    public bool BuildingSeize(GameObject gridKey, BaseBuilding building)
+    {
+        if (buildingsMap.TryGetValue(gridKey, out BaseBuilding b) && b == null)
+        {
+            buildingsMap[gridKey] = building;
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 释放建筑所占用的格子
+    /// </summary>
+    public bool BuildingRelease(BaseBuilding building)
+    {
+        GameObject gridKey = null;
+        foreach (var v in buildingsMap)
+        {
+            if (v.Value == building)
+            {
+                gridKey = v.Key;
+                break;
+            }
+        }
+
+        if (gridKey == null)
+            return false;
+
+        if (buildingsMap.TryGetValue(gridKey, out BaseBuilding b) && b == building)
+        {
+            buildingsMap[gridKey] = null;
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Utility/LayerUtility.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LayerUtility
+{
+    public static int Grid => 6;
+}
