12d0f381f2171bb152f0bd578da22cfb34cc25de
Owl Bai <1018679568@qq.com>
2022-10-26 16:34:24 +0800

    ammo

--- Assets/Scripts/Entity/Building/DefenseTower.cs
+++ Assets/Scripts/Entity/Building/DefenseTower.cs
@@ -6,17 +6,42 @@ using UnityEngine;
 public class DefenseTower : BaseBuilding
 {
     public Area attackArea;
+    public AmmoType attackType;
 
     public float damage;
     public float attackInterval;
     public float attackDistance;
-
+    public Ammo ammo;
+    
     public List<BaseEnemy> enemies = new List<BaseEnemy>();
+    private float _timer = 0;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (enemies.Count > 0 && _timer >= attackInterval)
+        {
+            _timer = 0;
+            Attack(enemies[0]);
+        }
+    }
+
+    private void UpdateEnemies()
+    {
+        for (int i = enemies.Count; i >= 0; i--)
+        {
+            if (enemies[i] == null)
+            {
+                enemies.RemoveAt(i);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(1111);
         BaseEnemy enemy = other.GetComponent<BaseEnemy>();
-        if (enemy)
+        if (enemy && enemy.area == attackArea)
         {
             enemies.Add(enemy);
         }
@@ -25,11 +50,26 @@ public class DefenseTower : BaseBuilding
     private void OnTriggerExit(Collider other)
     {
         BaseEnemy enemy = other.GetComponent<BaseEnemy>();
-        if (enemy)
+        if (enemy && enemy.area == attackArea)
         {
             enemies.Remove(enemy);
         }
     }
-    
-    
+
+    private void Attack(BaseEnemy enemy)
+    {
+        if (enemies[0] == null)
+        {
+            UpdateEnemies();
+        }
+
+        if (enemies.Count > 0)
+        {
+            Ammo bullet = Instantiate(ammo,transform);
+            bullet.damage = damage;
+            bullet.type = attackType;
+            bullet.originTower = this;
+            bullet.targetEnemy = enemy;
+        }
+    }
 }
--- Assets/Scripts/Entity/Bullet/Ammo.cs
+++ Assets/Scripts/Entity/Bullet/Ammo.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public DefenseTower originTower;
+    public BaseEnemy targetEnemy;
+    public AmmoType type;
+    public float speed;
+    public float damage;
+    
+    public GameObject explosionEffectPrefab;
+    
+    private float _minDist = 0.02f;
+
+    private void Update()
     {
+        if (!targetEnemy)
+        {
+            DestroyAmmo();
+        }
         
+        Vector3 dir = (targetEnemy.transform.position - transform.position).normalized;
+        transform.Translate(dir * Time.deltaTime * speed);
+        if (Vector2.Distance(targetEnemy.transform.position,transform.position)<_minDist)
+        {
+            if (type == AmmoType.AreaOfEffect)
+            {
+
+            }
+            targetEnemy.TakeDamage(damage);
+            DestroyAmmo();   
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyAmmo()
     {
-        
+        Destroy(gameObject);
     }
+
+    // public void SetOriginTower(DefenseTower tower)
+    // {
+    //     originTower = tower;
+    // }
+    //
+    // public void SetTargetEnemy(BaseEnemy enemy)
+    // {
+    //     targetEnemy = enemy;
+    // }
+}
+
+public enum AmmoType
+{
+    SingleAttack,
+    AreaOfEffect
 }
